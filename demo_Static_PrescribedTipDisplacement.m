1e97dc2e12aac31eeb1f8430a5011c21196547ab
Lawrence Smith <32077819+LawSmith408@users.noreply.github.com>
2025-08-04 09:14:46 +0200

    Edits to Prescribed Displacement Demo
    Added diameter grading to prescribed displacement demo

--- demo_Static_PrescribedTipDisplacement.m
+++ demo_Static_PrescribedTipDisplacement.m
@@ -12,9 +12,13 @@ W.fName = 'test01';             %[] analysis name
 
 %Whisker Dimensions and Material Properties
 W.Length = 25;                  %[mm] whisker length
-W.Diameter = 0.075;             %[mm] diameter of whisker
-W.E_root = 3340;                %[MPa] elastic modulus at root
 W.nEl = 25;                     %[] number of elements in whisker
+
+W.D_root = 0.075;             %[mm] diameter of whisker at root
+W.D_tip  = 0.075;             %[mm] diameter of whisker at tip
+
+W.E_root = 3340;                %[MPa] elastic modulus at root
+W.E_tip = 3340;               %[mm] elastic modulus at tip
 
 %Boundary Conditions - NOTE only one of these may be nonzero
 W.appliedMoment = 0;         %[N*mm] applied moment at tip
@@ -22,47 +26,16 @@ W.appliedForce = 0;             %[N] vertical force applied at tip
 W.prescribedDisp = 7;           %[mm] vertical displacement applied at tip
 
 %% Simulations
-%Simulate a nominal isotropic whisker
-W.E_tip = 3340;                 %[MPa] elastic modulus at tip
-[simDataISO] = simulateWhiskerStatic(W);
-cmd_rmdir(W.fName);
-
-%Reduce the modulus at the tip by a factor of 100 and re-simulate
+%Reduce the modulus at the tip by a factor of 100 and simulate
 W.E_tip = 33.4;                 %[MPa] elastic modulus at tip
 [simDataFGM] = simulateWhiskerStatic(W);
-cmd_rmdir(W.fName);
 
 %% Generate Figure
-
 figure;
 set(gcf,'position',[283.8000 272.2000 800 369.6000])
 colormap(autumn(20));
 axis(axisLim(simDataFGM.mesh.Points + simDataFGM.U(:,:,end))+[-1 8 0 2 -1 0])
-clim([min(simDataISO.S1(:,end)) max(simDataISO.S1(:,end))]);
-
-%plot and label ISO whisker
-V = simDataISO.mesh.Points + simDataISO.U(:,:,end);
-patch('Faces',simDataISO.mesh.Elements,'Vertices',V,'linewidth',3,...
-    'facevertexcdata',[simDataISO.S1(:,end); 0],'edgecolor','flat',...
-    'displayname','nonlinear FEA'); hold on
-
-c=colorbar('location','eastoutside');
-c.Label.String='\sigma_{11} [MPa]';
-set(gca,'fontsize',14)
-xlabel('X Pos. [mm]')
-ylabel('Y Pos. [mm]')
-grid on
-ylim([0 10])
-axis equal
-ylim([0 10])
-
-% text(V(floor(W.nEl/2),1)-2,V(floor(W.nEl/2),2),'HOM','fontsize',14,'HorizontalAlignment','center')
-
-figure;
-set(gcf,'position',[283.8000 272.2000 800 369.6000])
-colormap(autumn(20));
-axis(axisLim(simDataFGM.mesh.Points + simDataFGM.U(:,:,end))+[-1 8 0 2 -1 0])
-clim([min(simDataISO.S1(:,end)) max(simDataISO.S1(:,end))]);
+clim([min(simDataFGM.S1(:,end)) max(simDataFGM.S1(:,end))]);
 
 %plot and label FGM whisker
 V = simDataFGM.mesh.Points + simDataFGM.U(:,:,end) + [5 0 0];
@@ -82,22 +55,4 @@ ylim([0 10])
 axis equal
 ylim([0 10])
 
-% %add inset axis showing modulus variation
-% small_ax = axes('position',[0.2 0.56 0.3 0.3]);
-% plot((0:W.nEl-1)/W.nEl,simDataISO.S1(:,end),'k--','LineWidth',1.5,'DisplayName','ISO'); hold on
-% plot((0:W.nEl-1)/W.nEl,simDataFGM.S1(:,end),'k-','LineWidth',1.5,'DisplayName','FGM'); hold on
-% legend('location','northeast')
-% xlabel(small_ax,'x/L')
-% ylabel(small_ax,'\sigma_{11} [MPa]')
-% set(small_ax,'fontsize',12)
-
-% %add inset axis showing modulus variation
-% small_ax = axes('position',[0.23 0.56 0.3 0.3]);
-% plot([0 1],[1 1],'k--','LineWidth',1.5,'DisplayName','HOM'); hold on
-% plot([0 1],[1 1e-2],'k-','LineWidth',1.5,'DisplayName','FGM'); hold on
-% legend('location','northeast')
-% xlabel(small_ax,'x/L')
-% ylabel(small_ax,'E(x)/E_{root}')
-% set(small_ax,'YScale','log')
-% set(small_ax,'fontsize',12)
 
--- dep/simulateWhiskerStatic.m
+++ dep/simulateWhiskerStatic.m
@@ -11,7 +11,7 @@ mkdir(W.fName); cd(W.fName);
 
 % fprintf('\n\nElement Aspect Ratio: %1.3f\n\n',W.Length/(W.Diameter*W.nEl));
 
-mesh.AspectRatio = W.Length/(W.Diameter*W.nEl);
+% mesh.AspectRatio = W.Length/(W.Diameter*W.nEl);
 
 %define mesh nodes
 mesh.Points = [linspace(0,W.Length,W.nEl+1)' zeros(W.nEl+1,2) ];
@@ -44,17 +44,31 @@ for i = 1:size(mesh.Elements,1)
    fprintf(fileID,formatSpec,line);
 end
 
+%% NEED TO MAKE INDIVIDUAL EL SETS AND MATERIALS
+
 %Write Nodesets
 fprintf(fileID,'*Nset, nset=WholeModel, generate\n');
 fprintf(fileID,'  1,  %i,   1\n',W.nEl+1);
 fprintf(fileID,'*Elset, elset=WholeModel, generate\n');
 fprintf(fileID,'  1,  %i,   1\n',W.nEl);
+
+%write element sets
+for i = 1:W.nEl
+fprintf(fileID,'*Elset, elset=EL_%i\n%i,\n',i,i);
+end
+
 fprintf(fileID,'*Orientation, name=Ori-1\n');
 fprintf(fileID,'1., 0., 0., 0., 1., 0.\n');
 fprintf(fileID,'1, 0.\n');
-fprintf(fileID,'*Beam Section, elset=WholeModel, material=LinearElasticGraded, temperature=GRADIENTS, section=CIRC\n');
-fprintf(fileID,'%1.5f\n',W.Diameter/2);
+
+%write sections
+for i = 1:W.nEl
+    phi = (i-1)/(W.nEl-1);
+fprintf(fileID,'*Beam Section, elset=EL_%i, material=Mat_%i, temperature=GRADIENTS, section=CIRC\n',i,i);
+fprintf(fileID,'%1.6f\n',(phi*(W.D_tip - W.D_root) + W.D_root)*0.5);
 fprintf(fileID,'0.,0.,-1.\n');
+end
+
 fprintf(fileID,'*End Part\n');
 
 fprintf(fileID,'*Assembly, name=Assembly\n');
@@ -66,10 +80,14 @@ fprintf(fileID,'*Nset, nset=Tip, instance=Whisker-1\n');
 fprintf(fileID,' %i,\n',W.nEl+1);
 fprintf(fileID,'*End Assembly\n');
 
-fprintf(fileID,'*Material, name=LinearElasticGraded\n');
-fprintf(fileID,'*Elastic, dependencies=1\n');
-fprintf(fileID,'%1.1f, 0.4, ,  0.\n',W.E_root);
-fprintf(fileID,'%1.1f, 0.4, ,  1.\n',W.E_tip);
+for i = 1:W.nEl
+phi = (i-1)/(W.nEl-1);
+fprintf(fileID,'*Material, name=Mat_%i\n',i);
+% fprintf(fileID,'*Density\n');
+% fprintf(fileID,'%2.2e,\n',    phi*  (W.rho_tip - W.rho_root) + W.rho_root);
+fprintf(fileID,'*Elastic\n');
+fprintf(fileID,'%2.2f, 0.4\n',phi*  (W.E_tip   - W.E_root)   + W.E_root);
+end
 
 fprintf(fileID,'*Boundary\n');
 fprintf(fileID,'Root, ENCASTRE\n');
@@ -87,10 +105,10 @@ fprintf(fileID,'*Boundary\n');
 fprintf(fileID,'Tip, 2, 2, %1.6f\n',W.prescribedDisp);
 end
 
-fprintf(fileID,'*Field, variable=1\n');
-for i = 1:W.nEl+1
-fprintf(fileID,'Whisker-1.%i, %1.3f\n',i,(i-1)/W.nEl);
-end
+% fprintf(fileID,'*Field, variable=1\n');
+% for i = 1:W.nEl+1
+% fprintf(fileID,'Whisker-1.%i, %1.3f\n',i,(i-1)/W.nEl);
+% end
 
 
 fprintf(fileID,'*Restart, write, frequency=0\n');
